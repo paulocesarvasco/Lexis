753e1cf7ba5708a184c3cc31f4f6ec9c240fbe7b
paulocesarvasco <paulocvasco.93@gmail.com>
2021-07-01 00:01:11 -0300

    Create main class

---
+++ Lexis/src/com/lexis/Main.java
@@ -0,0 +1,12 @@
+package com.lexis;
+
+import com.lexis.utils.ReadFile;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		
+		ReadFile.getMatrix(args[0]);
+		
+	}
+}
