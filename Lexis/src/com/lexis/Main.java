28edb9ede1c8bb2796b1340b18b4716497e3624a
paulocesarvasco <paulocvasco.93@gmail.com>
2021-07-01 01:22:56 -0300

    Add logic to read each position of the matrix and zeroize it when necessary

--- Lexis/src/com/lexis/Main.java
+++ Lexis/src/com/lexis/Main.java
@@ -6,7 +6,23 @@ public class Main {
 
 	public static void main(String[] args) throws Exception {
 		
-		ReadFile.getMatrix(args[0]);
+		Matrix matrix = ReadFile.getMatrixFromFile(args[0]);
+		
+		if (matrix == null) {
+			System.out.println("Error to read matrix file");
+			return;
+		}
+		char[][] tmp_mtx = matrix.getCopy();
+		
+		for (int i = 0; i < matrix.getM(); i++) {
+			for (int j = 0; j < matrix.getN(); j++) {
+				if (tmp_mtx[i][j] == '0') {
+					matrix.zeroizePosition(i, j);
+				}
+			}
+		}
+		
+		matrix.printMatrix();
 		
 	}
 }
